c6eb6fcc51c73d128a386f46ab3ae46d6a9d6263
chang chang <changhammer5499@gmail.com>
2023-06-18 12:05:01 +0700

    stupid

---
+++ src/main/java/com/axonactive/PersonalProject/finecalculator/PaymentGatewayAdapter.java
@@ -0,0 +1,14 @@
+package com.axonactive.PersonalProject.finecalculator;
+
+public class PaymentGatewayAdapter implements PaymentGateway {
+    private final FineCalculator fineCalculator;
+
+    public PaymentGatewayAdapter(FineCalculator fineCalculator) {
+        this.fineCalculator = fineCalculator;
+    }
+
+    @Override
+    public Double processPayment(Long numberOfDays) {
+        return fineCalculator.calculatingFineFee(numberOfDays);
+    }
+}
